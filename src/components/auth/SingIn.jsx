12c9d3734d58ffaa10f0d093f040d0ba5a00e41e
zeleznakovs <138462848+zeleznakovs@users.noreply.github.com>
2024-03-20 18:17:51 +0200

    Update SingIn.jsx

--- src/components/auth/SingIn.jsx
+++ src/components/auth/SingIn.jsx
@@ -6,6 +6,8 @@ const SignIn = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [showPassword, setShowPassword] = useState(false); // Додано стан для відображення / приховування пароля
+
   function logIn(e) {
     e.preventDefault();
     signInWithEmailAndPassword(auth, email, password)
@@ -15,11 +17,12 @@ const SignIn = () => {
         setEmail("");
         setPassword("");
       })
-        .catch((error) => {
-            console.log(error);
-            setError("SORRY, COULDN'T FIND YOUR ACCOUNT")
+      .catch((error) => {
+        console.log(error);
+        setError("SORRY, COULDN'T FIND YOUR ACCOUNT");
       });
   }
+
   return (
     <div>
       <form>
@@ -34,9 +37,17 @@ const SignIn = () => {
           placeholder="Please enter your password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          type="password"
+          type={showPassword ? "text" : "password"} // Змінено тип поля введення залежно від стану showPassword
         />
         <button onClick={logIn}>Login</button>
+        <label>
+          <input
+            type="checkbox"
+            checked={showPassword}
+            onChange={() => setShowPassword(!showPassword)}
+          />
+          Show Password
+        </label>
         {error ? <p style={{ color: "red" }}>{error}</p> : ""}
       </form>
     </div>
